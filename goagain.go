7c621b699d5496c2c2488d9908874dda87a86ee4
Joshua Marsh (icub3d) <joshua@themarshians.com>
2013-08-25 09:09:12 -0600

    Added the ability to call a user defined function for the SIGHUP and SIGUSR1 signals.

--- goagain.go
+++ goagain.go
@@ -13,6 +13,18 @@ import (
 	"syscall"
 )
 
+var (
+	// OnSigHup is the function called when the server receives a SIGHUP
+	// signal. The normal use case for SIGHUP is to reload the
+	// configuration.
+	OnSigHup func(l net.Listener) error
+
+	// OnSigUsr1 is the function called when the server receives a
+	// SIGUSR1 signal. The normal use case for SIGUSR1 is to repon the
+	// log files.
+	OnSigUsr1 func(l net.Listener) error
+)
+
 // Export an error equivalent to net.errClosing for use with Accept during
 // a graceful exit.
 var ErrClosing = errors.New("use of closed network connection")
@@ -28,7 +40,14 @@ func AwaitSignals(l net.Listener) error {
 		log.Println(sig.String())
 		switch sig {
 
-		// TODO SIGHUP should reload configuration.
+		// SIGHUP should reload configuration.
+		case syscall.SIGHUP:
+			if OnSigHup != nil {
+				err := OnSigHup(l)
+				if err != nil {
+					log.Println("result of OnSigHup:", err)
+				}
+			}
 
 		// SIGQUIT should exit gracefully.
 		case syscall.SIGQUIT:
@@ -38,7 +57,14 @@ func AwaitSignals(l net.Listener) error {
 		case syscall.SIGTERM:
 			return nil
 
-		// TODO SIGUSR1 should reopen logs.
+		// SIGUSR1 should reopen logs.
+		case syscall.SIGUSR1:
+			if OnSigUsr1 != nil {
+				err := OnSigUsr1(l)
+				if err != nil {
+					log.Println("result of OnSigUsr1:", err)
+				}
+			}
 
 		// SIGUSR2 begins the process of restarting without dropping
 		// the listener passed to this function.
@@ -51,7 +77,7 @@ func AwaitSignals(l net.Listener) error {
 		}
 	}
 
-        return nil
+	return nil
 }
 
 // Convert and validate the GOAGAIN_FD, GOAGAIN_NAME, and GOAGAIN_PPID
