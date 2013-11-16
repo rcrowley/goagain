b1481615c2e7b7afb80a96b519e3ded9ed809534
Richard Crowley <r@rcrowley.org>
2013-11-16 18:46:38 +0000

    Support the old API in terms of the new one.

--- test.sh
+++ test.sh
@@ -9,26 +9,29 @@ findproc() {
     set -x
 }
 
-cd "example/single"
-go build
-./single &
-PID="$!"
-[ "$PID" -a -d "/proc/$PID" ]
-for _ in _ _
+for NAME in "legacy" "single"
 do
-    OLDPID="$PID"
-    sleep 1
-    kill -USR2 "$PID"
+    cd "example/$NAME"
+    go build
+    ./$NAME &
+    PID="$!"
+    [ "$PID" -a -d "/proc/$PID" ]
+    for _ in _ _
+    do
+        OLDPID="$PID"
+        sleep 1
+        kill -USR2 "$PID"
+        sleep 2
+        PID="$(findproc "$NAME")"
+        [ ! -d "/proc/$OLDPID" -a "$PID" -a -d "/proc/$PID" ]
+    done
+    [ "$(nc "127.0.0.1" "48879")" = "Hello, world!" ]
+    kill -TERM "$PID"
     sleep 2
-    PID="$(findproc "single")"
-    [ ! -d "/proc/$OLDPID" -a "$PID" -a -d "/proc/$PID" ]
+    [ ! -d "/proc/$PID" ]
+    [ -z "$(findproc "$NAME")" ]
+    cd "$OLDPWD"
 done
-[ "$(nc "127.0.0.1" "48879")" = "Hello, world!" ]
-kill -TERM "$PID"
-sleep 2
-[ ! -d "/proc/$PID" ]
-[ -z "$(findproc "single")" ]
-cd "$OLDPWD"
 
 cd "example/double"
 go build
